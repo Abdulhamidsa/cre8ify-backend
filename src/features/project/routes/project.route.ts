92fb1faccf905a5c7e11959dc5e93cea9875fd0e
Abdulhamid <alsaati.abdulhamid@gmail.com>
2024-12-21 13:45:47 +0100

    add project route for fetching a specific project by ID

---
+++ src/features/project/routes/project.route.ts
@@ -0,0 +1,9 @@
+// import { Router } from "express";
+// import { handleFetchAllProjects } from "../handlers/project.handler";
+
+// const router = Router();
+
+// // fetch all projects
+// router.get("/:projectid", handleFetchAllProjects);
+
+// export default router;
