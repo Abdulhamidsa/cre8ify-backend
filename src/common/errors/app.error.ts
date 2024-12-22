d917e030f6e5b71c986e89dae048dc12e086b20b
Abdulhamid <alsaati.abdulhamid@gmail.com>
2024-12-22 03:54:10 +0100

    refactor error handling: improve AppError constructor and enhance express error middleware with logging and dynamic status codes

--- src/common/errors/app.error.ts
+++ src/common/errors/app.error.ts
@@ -1,9 +1,7 @@
 export class AppError extends Error {
   public status: number;
-
-  constructor(message: string, status: number) {
+  constructor(message: string, status = 400) {
     super(message);
     this.status = status;
-    Object.setPrototypeOf(this, AppError.prototype);
   }
 }
--- src/common/middleware/error.middleware.ts
+++ src/common/middleware/error.middleware.ts
@@ -1,18 +1,23 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
 import { AppError } from "../errors/app.error";
+import { getErrorMessage } from "../utils/error.utils";
+import Logger from "../utils/logger";
 
-const expressErrorMiddleware = (err: Error, _req: Request, res: Response, _next: NextFunction): Response => {
-  console.error(err);
+const expressErrorMiddleware: ErrorRequestHandler = (err: AppError | Error, _req: Request, res: Response, _next: NextFunction): void => {
+  const isDev = process.env.NODE_ENV !== "production";
+  const statusCode = err instanceof AppError ? err.status : 500;
+  const message = getErrorMessage(err);
 
-  if (err instanceof AppError) {
-    return res.status(err.status).json({
-      success: false,
-      message: err.message,
-    });
+  // Log the error
+  Logger.error(message); // Logs to console and file
+  if (isDev && err.stack) {
+    Logger.debug(`Stack Trace:\n${err.stack}`);
   }
-  return res.status(500).json({
+
+  // Send response
+  res.status(statusCode).json({
     success: false,
-    message: "Internal Server Error",
+    message,
   });
 };
 
