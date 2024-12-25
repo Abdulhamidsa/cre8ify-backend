3d28b25afaa50b04b0f187610b9ea95626b097dc
Abdulhamid <alsaati.abdulhamid@gmail.com>
2024-12-25 09:26:10 +0100

    refactor mongo connection: standardize string quotes and improve import order

--- src/common/config/mongo.connection.ts
+++ src/common/config/mongo.connection.ts
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
-import { SECRETS } from "../config/config";
-import Logger from "../utils/logger";
-import { AppError } from "../errors/app.error";
-import { getErrorMessage } from "../utils/error.utils";
+import mongoose from 'mongoose';
+
+import { SECRETS } from '../config/config';
+import { AppError } from '../errors/app.error';
+import { getErrorMessage } from '../utils/error.utils';
+import Logger from '../utils/logger';
 
 export const connectMongoDB = async () => {
   try {
     await mongoose.connect(SECRETS.mongoConnectionString, {
       maxPoolSize: 10,
     });
-    Logger.info("MongoDB connected successfully");
   } catch (error) {
     const errorMessage = getErrorMessage(error);
     Logger.error(`MongoDB connection failed: ${errorMessage}`);
-    throw new AppError("Database connection error", 500);
+    throw new AppError('Database connection error', 500);
   }
 };
