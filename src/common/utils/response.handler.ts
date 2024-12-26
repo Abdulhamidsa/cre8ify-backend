06fa6eb458fc8c57f104fbf8ccf7ee03bc739597
Abdulhamid <alsaati.abdulhamid@gmail.com>
2024-12-26 02:46:14 +0100

    refactor: streamline response handling by consolidating response types into ApiResponse

--- src/common/utils/response.handler.ts
+++ src/common/utils/response.handler.ts
@@ -1,21 +1,3 @@
-// export type SuccessResponse<T> = {
-//   success: boolean;
-//   data: T;
-// };
-
-// export type ErrorResponse = {
-//   success: boolean;
-//   message: string;
-// };
-// export const getSuccessResponse = <T>(data: T): SuccessResponse<T> => ({
-//   success: true,
-//   data,
-// });
-// export const getErrorResponse = (message: string): ErrorResponse => ({
-//   success: false,
-//   message,
-// });
-
 export type ApiResponse<T> = {
   success: boolean;
   data?: T;
