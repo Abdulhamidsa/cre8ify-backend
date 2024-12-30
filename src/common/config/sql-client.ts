54bd1f53b8f2729182eab24f0ca91ff33284b09f
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2024-12-30 13:20:54 +0100

    feat: add table creation logic for users and enhance PostgreSQL health check

--- src/common/config/sql-client.ts
+++ src/common/config/sql-client.ts
@@ -2,16 +2,17 @@ import { Pool, PoolClient } from 'pg';
 
 import { AppError } from '../errors/app.error';
 import { getErrorMessage } from '../utils/error.utils';
+import Logger from '../utils/logger';
+import { SQL_QUERIES } from '../utils/sql.constants';
 import { SECRETS } from './config';
 
 // SSL Configuration based on environment
-// SSL Configuration
 const sslConfig = SECRETS.nodeEnv === 'production' ? { rejectUnauthorized: true } : false;
 
 // Create the pool instance
 const pool = new Pool({
   connectionString: SECRETS.postgresConnectionString,
-  ssl: sslConfig, // Use 'false' to disable SSL for development
+  ssl: sslConfig,
 });
 
 // Function to get a client on demand
@@ -24,16 +25,34 @@ export const getSQLClient = async (): Promise<PoolClient> => {
   }
 };
 
+// Function to check and create tables if they don't exist
+export const ensureTablesExist = async (): Promise<void> => {
+  const sqlClient = await getSQLClient();
+
+  const tableQueries = [SQL_QUERIES.createUsersTable];
+
+  try {
+    Logger.info('Ensuring database tables exist...');
+    for (const query of tableQueries) {
+      await sqlClient.query(query);
+    }
+    Logger.info('All necessary tables are ensured.');
+  } catch (error) {
+    Logger.error('Error ensuring tables exist:', error);
+    throw new AppError('Database initialization failed', 500);
+  } finally {
+    sqlClient.release();
+  }
+};
+
 // Health check function for PostgreSQL
 export const checkPostgresHealth = async (): Promise<void> => {
   try {
     const client = await getSQLClient();
-    await client.query('SELECT 1');
+    await client.query('SELECT NOW()');
     client.release();
-    console.log('PostgreSQL connection is healthy');
   } catch (error) {
-    console.error('PostgreSQL health check failed:', getErrorMessage(error));
-    throw new AppError('PostgreSQL health check failed', 500);
+    throw new AppError(getErrorMessage(error), 500);
   }
 };
 
--- src/common/utils/sql.constants.ts
+++ src/common/utils/sql.constants.ts
@@ -7,4 +7,13 @@ export const SQL_QUERIES = {
   rollbackUser: 'DELETE FROM users WHERE id = $1;',
   getUserLogin: 'SELECT password_hash, mongo_ref FROM users WHERE username = $1;',
   deleteUser: 'DELETE FROM users WHERE mongo_ref = $1;',
+
+  createUsersTable: `
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(100) UNIQUE NOT NULL,
+      password_hash TEXT NOT NULL,
+      mongo_ref VARCHAR(100) UNIQUE NOT NULL
+    );
+  `,
 };
