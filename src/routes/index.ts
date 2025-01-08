79f55b65d3855ce74d9ccd046cedd38d56befbaa
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-01-08 18:54:33 +0100

    feat: add public routes for project retrieval

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,12 +6,14 @@ import authPrivateRoutes from '../features/auth/routes/auth.private.routes.js';
 import authPublicRoutes from '../features/auth/routes/auth.public.routes.js';
 import postPrivateRoutes from '../features/post/routes/post.private.routes.js';
 import projectPrivateRoutes from '../features/project/routes/projects.private.routes.js';
+import projectsPublicRoutes from '../features/project/routes/projects.public.routes.js';
 import userPrivateRoutes from '../features/user/routes/user.private.routes.js';
 
 const router = Router();
 
 // Public routes
 router.use('/auth', authPublicRoutes);
+router.use(projectsPublicRoutes);
 
 // Private routes (common middlewares)
 router.use(authenticateAndRefresh, attachUserContext);
